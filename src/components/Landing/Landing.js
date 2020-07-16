a31b54fda4356c5f3b5be4d159acee249b795fd5
TecDvr <zach.gwirtz@datadoghq.com>
2020-07-16 12:29:20 -0700

    landing update

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,9 +6,18 @@ export default class Landing extends React.Component {
 
     render() {
         return (
-            <div>
-                <p>{this.context.test.title}</p>
-                <p>{this.context.test.id}</p>
+            <div className='landing-container'>
+                <div className='title'>
+                    <h1>dummydog</h1>
+                    <p>info sent the right way</p>
+                </div>
+                <div className='api'>
+                    
+                </div>
+                <div>
+                    <p>{this.context.test.title}</p>
+                    <p>{this.context.test.id}</p>
+                </div>
             </div>
         )
     }
