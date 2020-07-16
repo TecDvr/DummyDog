dc9d91f38299285e683596626e432d231e5c52a6
TecDvr <zach.gwirtz@datadoghq.com>
2020-07-16 14:21:30 -0700

    send custom logs

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -8,18 +8,27 @@ export default class Landing extends React.Component {
         super(props);
         this.state = {
             api: null,
-            error: null
+            error: null,
+            logBody: {
+                ddsource: 'testsource',
+                ddtags: 'env:staging',
+                hostname: 'mothership',
+                message: '2019-11-19T14:37:58,995 INFO [process.name][20081] Hello World'
+            }
         }
+        this.handleLogSend = this.handleLogSend.bind(this);
     }
 
     handleLogSend(e) {
+        console.log('workin hard or hardly working?');
         e.preventDefault();
         fetch(`${config.LOGS_ENDPOINT}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'DD-API-KEY': `${this.state.api}`
-            }
+                'DD-API-KEY': `${this.state.api}`,
+            },
+            body: JSON.stringify(this.state.logBody)
         })
         .then(res =>
             (res.ok) 
@@ -48,6 +57,68 @@ export default class Landing extends React.Component {
                         id='apikey'
                         onChange={e => this.setState({ api: e.target.value })}>
                     </input>
+
+                    <label className='source-input-label' htmlFor='source'>Log Source</label>
+                    <input
+                        className='source-input'
+                        required
+                        placeholder='source'
+                        type='text'
+                        name='source'
+                        id='source'
+                        onChange={e => {
+                            let logBody = {...this.state.logBody}
+                            logBody.ddsource = e.target.value;
+                            this.setState({ logBody })
+                        }}>
+                    </input>
+
+                    <label className='tag-input-label' htmlFor='tag'>Log Tag</label>
+                    <input
+                        className='tag-input'
+                        required
+                        placeholder='tags'
+                        type='text'
+                        name='tag'
+                        id='tag'
+                        onChange={e => {
+                            let logBody = {...this.state.logBody}
+                            logBody.ddtags = e.target.value;
+                            this.setState({ logBody })
+                        }}>
+                    </input>
+
+                    <label className='hostname-input-label' htmlFor='hostname'>Log Hostname</label>
+                    <input
+                        className='hostname-input'
+                        required
+                        placeholder='hostname'
+                        type='text'
+                        name='hostname'
+                        id='hostname'
+                        onChange={e => {
+                            let logBody = {...this.state.logBody}
+                            logBody.hostname = e.target.value;
+                            this.setState({ logBody })
+                        }}>
+                    </input>
+
+                    <label className='message-input-label' htmlFor='message'>Log Message</label>
+                    <input
+                        className='message-input'
+                        required
+                        placeholder='message'
+                        type='text'
+                        name='message'
+                        id='message'
+                        onChange={e => {
+                            let logBody = {...this.state.logBody}
+                            logBody.message = e.target.value;
+                            this.setState({ logBody })
+                        }}>
+                    </input>
+
+                    <button onClick={this.handleLogSend}></button>
                 </form>
             </div>
         )
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
     API_ENDPOINT: 'https://jsonplaceholder.typicode.com/todos/1',
-    LOGS_ENDPOINT: 'https://http-intake.logs.datadoghq.com/v1/input'
+    LOGS_ENDPOINT: 'https://cors-anywhere.herokuapp.com/https://http-intake.logs.datadoghq.com/v1/input'
 }
