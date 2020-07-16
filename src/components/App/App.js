39ecfe6054173c135b89e59d8f149f727bc1a7ab
TecDvr <zach.gwirtz@datadoghq.com>
2020-07-16 08:33:58 -0700

    basic layout

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className='app-container'>
+        <p>dummydog</p>
+      </div>
+    )
+  }
+}
