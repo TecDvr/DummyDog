6149e7a1192d7f8101983f55046e94454117ec51
TecDvr <zach.gwirtz@datadoghq.com>
2020-07-16 13:17:59 -0700

    send custom logs

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import DummydogContext from '../../context/dummydog-context'
+import config from '../../config';
 
 export default class Landing extends React.Component {
     static contextType = DummydogContext;
+    constructor(props) {
+        super(props);
+        this.state = {
+            api: null,
+            error: null
+        }
+    }
+
+    handleLogSend(e) {
+        e.preventDefault();
+        fetch(`${config.LOGS_ENDPOINT}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'DD-API-KEY': `${this.state.api}`
+            }
+        })
+        .then(res =>
+            (res.ok) 
+                ? res.json().then(allGood => {
+                console.log(allGood)
+            })
+            : res.json().then(resJson=>this.setState({error:resJson.error}))
+        )  
+    }
 
     render() {
         return (
@@ -11,13 +37,18 @@ export default class Landing extends React.Component {
                     <h1>dummydog</h1>
                     <p>info sent the right way</p>
                 </div>
-                <div className='api'>
-                    
-                </div>
-                <div>
-                    <p>{this.context.test.title}</p>
-                    <p>{this.context.test.id}</p>
-                </div>
+                <form className='api'>
+                    <label className='api-input-label' htmlFor='apikey'>Your API Key</label>
+                    <input
+                        className='api-input'
+                        required
+                        placeholder='Your API Key'
+                        type='text'
+                        name='apikey'
+                        id='apikey'
+                        onChange={e => this.setState({ api: e.target.value })}>
+                    </input>
+                </form>
             </div>
         )
     }
--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://jsonplaceholder.typicode.com/todos/1'
+    API_ENDPOINT: 'https://jsonplaceholder.typicode.com/todos/1',
+    LOGS_ENDPOINT: 'https://http-intake.logs.datadoghq.com/v1/input'
 }
