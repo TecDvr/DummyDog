53815d666be2ce4cb742a742371e558ff8ccf54b
ajsaavedra <tony.saavedra@datadoghq.com>
2020-07-16 15:51:49 -0700

    Dockerizing backend, connecting to DB

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,13 +1,20 @@
 import * as express from "express";
+import { connection } from "./db";
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log("Hola...");
+    connection.query("SELECT * FROM SendLogsBodyData", (error, results, fields) => {
+        if (error) {
+            res.status(400).send("Could not query DB");
+            throw error;
+        }        
+        res.status(200).send(results);
+    });
 });
 
 router.get('/data', (req, res) => {
-    console.log("DATA");
+    res.status(200).send('Data');
 });
 
 export { router };
