9e8021be7da1ee8960636fd9a12d9129b5d1cf6b
TecDvr <zach.gwirtz@datadoghq.com>
2020-07-16 11:44:11 -0700

    router setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Landing from '../Landing/Landing'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      test: ''
     }
   }
+
   render() {
     return (
       <div className='app-container'>
-        <p>dummydog</p>
+     
+          <Route
+            exact
+            path='/'
+            component={Landing}>
+          </Route>
+
       </div>
     )
   }
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Landing extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>test</p>
+            </div>
+        )
+    }
+}
