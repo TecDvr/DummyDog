f2ef3cc57150de75f53ab95015709cb2a6608195
roseannam <roseanna.mcfarlane@datadoghq.com>
2020-07-16 18:29:05 -0700

    dropdowns done

--- src/components/RealLanding/RealLanding.js
+++ src/components/RealLanding/RealLanding.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import Select from 'react-select'
 
-export default class RealLanding extends React.Component {
+export default withRouter(class RealLanding extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -16,12 +17,27 @@ export default class RealLanding extends React.Component {
                     <h1>dummydog</h1>
                     <p>info sent the right way</p>
                 </header>
-                <Link 
-                    style={{ textDecoration: 'none' }} 
-                    to='/landing'>
-                        <button className='linkButton'>Send Custom Logs</button>
-                </Link>
+                <Select onChange={(option) => this.props.history.push(option.value)} 
+                    defaultValue={{label: "Create New"}}
+                    options={[
+                        { label: "Logs", value: "logs" },
+                        { label: "Metrics", value: "metrics" },
+                    ]}
+                    styles={{ 
+                        control: (provided) => ({ ...provided, backgroundColor: "#3C00B2"}),
+                        singleValue: (provided) => ({...provided, color: "white"})
+                    }}
+                />
+                <Select onChange={(option) => this.props.history.push(option.value)} 
+                    defaultValue={{label: "Saved"}}
+                    options={[
+                    ]}
+                    styles={{ 
+                        control: (provided) => ({ ...provided, backgroundColor: "#0052B2"}),
+                        singleValue: (provided) => ({...provided, color: "white"})
+                    }}
+                />
             </div>
         )
     }
-}
+})
