821ddb48632fb039a1ac1683da719f3dfbaa6152
ajsaavedra <tony.saavedra@datadoghq.com>
2020-07-16 18:58:09 -0700

    Adding log templates and route to query them

--- backend/DummyDogSchema.sql
+++ backend/DummyDogSchema.sql
@@ -20,9 +20,8 @@ USE `mydb` ;
 DROP TABLE IF EXISTS `mydb`.`SendLogsBodyData` ;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`SendLogsBodyData` (
-  `idx` INT(3) NOT NULL,
-  `Content-Encoding` VARCHAR(45) NULL,
-  `ddsourse` VARCHAR(500) NULL,
+  `idx` INT(3) NOT NULL AUTO_INCREMENT,
+  `ddsource` VARCHAR(500) NULL,
   `ddtags` VARCHAR(500) NULL,
   `hostname` VARCHAR(45) NULL,
   `message` VARCHAR(1000) NULL,
@@ -30,6 +29,33 @@ CREATE TABLE IF NOT EXISTS `mydb`.`SendLogsBodyData` (
   PRIMARY KEY (`idx`))
 ENGINE = InnoDB;
 
+-- -----------------------------------------------------
+-- Table `mydb`.`LogsTemplateData`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `mydb`.`LogsTemplateData` (
+  `idx` INT(3) NOT NULL AUTO_INCREMENT,
+  `ddsource` VARCHAR(500) NULL,
+  `ddtags` VARCHAR(500) NULL,
+  `hostname` VARCHAR(45) NULL,
+  `message` VARCHAR(1000) NULL,
+  PRIMARY KEY (`idx`))
+ENGINE = InnoDB;
+
+INSERT INTO LogsTemplateData(ddsource, ddtags, hostname, message)
+VALUES
+  ("csharp", "env:staging, application:console", "i-012345678", "2017-03-10 15:13:53.130 ERROR [10] ConsoleApplication1.Program mymethod:32 - an error occured while processing"),
+  ("docker", "app:dummydog", "localhost", '172.17.0.1 - - [06/Jan/2017:16:16:37 +0000] "GET /datadoghq/company?test=var1%20Pl HTTP/1.1" 200 612 "http://www.perdu.com/" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36" "-"'),
+  ("iis", "service:demo,item:third", "i-928734421", "2002-05-02 17:42:15 172.22.255.255 GET /images/picture.jpg ?toto=tata 80 toto 123.123.123.123 Mozilla/4.0+(compatible;MSIE+5.5;+Windows+2000+Server) 200 211 322 1234"),
+  ("java", "cluster:javapp,env:prod,version:1.8", "cluster01-name", "2000-09-07 14:07:41,508 [main] INFO  MyApp - Entering application."),
+  ("nginx", "", "server-12345", '2019-11-19T14:37:58,995 INFO [process.name][20081] Hello World'),
+  ("nodejs", "service:payment,env:staging", "i-182730465", "Starting the staging server..."),
+  ("php", "", "worker187-php", 'Error message. [dd.span_id="4014120331748607290" dd.trace_id="2762343115747197096"]'),
+  ("python", "lang:python, service:tester, version:1", "host01-cluster2b-py", "2017-12-19T14:37:58,995 INFO  [process.name] [20081] this is my python log"),
+  ("redis", "", "server.redis.01", "12115:M 08 Jan 17:45:41.572 # Server started, Redis version 3.0.6"),
+  ("ruby", "service:api", "i-274583170",'I, [1999-03-03T02:34:24.895701 #19074]  INFO -- : Started GET "/api/v1/example/query" for 123.123.123.123 at 1999-03-03 02:34:24+0000'),
+  ("tomcat", "service:app", "vm-001-app", "2000-09-07 14:07:44 INFO org.foo.bar:32 - Entering application."),
+  ("zookeeper", "", "i-432156780", "2000-09-07 14:07:41,508 [main] INFO  MyApp - Entering application.");
 
 -- -----------------------------------------------------
 -- Table `mydb`.`GetLogsBodyData`
@@ -37,7 +63,7 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `mydb`.`GetLogsBodyData` ;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`GetLogsBodyData` (
-  `idx` INT NOT NULL,
+  `idx` INT NOT NULL AUTO_INCREMENT,
   `index` VARCHAR(100) NULL,
   `limit` INT(32) NULL,
   `query` VARCHAR(45) NOT NULL,
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,18 +3,25 @@ import { connection } from "./db";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    connection.query("SELECT * FROM SendLogsBodyData", (error, results, fields) => {
-        if (error) {
-            res.status(400).send("Could not query DB");
-            throw error;
-        }        
+router.get('/logs', (req, res) => {
+    connection.query("SELECT * FROM GetLogsBodyData", (error, results) => {
+        handleError(error, res);
         res.status(200).send(results);
     });
 });
 
-router.get('/data', (req, res) => {
-    res.status(200).send('Data');
+router.get('/templates', (req, res) => {
+    connection.query("SELECT * FROM LogsTemplateData", (error, results) => {
+        handleError(error, res);
+        res.status(200).send(results);
+    });
 });
+
+const handleError = (error, res) => {
+    if (error) {
+        res.status(400).send("Could not query DB");
+        throw error;
+    }
+};
 
 export { router };
