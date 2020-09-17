ed997abc4be3b0ac490a296851e201c4a719d651
TecDvr <zach.gwirtz@datadoghq.com>
2020-09-17 11:30:17 -0700

    event build

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,9 +1,54 @@
 import React from 'react';
-import DummydogContext from '../../context/dummydog-context'
+// import DummydogContext from '../../context/dummydog-context'
 import { Link } from 'react-router-dom';
+import config from '../../config';
 
 export default class Event extends React.Component {
-    static contextType = DummydogContext;
+    // static contextType = DummydogContext;
+    constructor(props) {
+        super(props);
+        this.state = {
+            api: '9e6a1bb4e18e7bbad6baa69826bfd34b',
+            eventBody: {
+                aggregation_key: null,
+                alert_type: 'info',
+                date_happened: null,
+                device_name: null,
+                host: 'mothership',
+                title: 'test',
+                text: 'ohhhhhhh yeahhhhhh bruh',
+                tags: [
+                    'key:value',
+                    'key2:value2'
+                ],
+                priority: 'normal',
+                source_type_name: 'kubernetes'
+            }
+        }
+        this.handleLogEvent = this.handleLogEvent.bind(this);
+    }
+
+    handleLogEvent(e) {
+        // this.setState({ loading: true })
+        e.preventDefault();
+        fetch(`${config.EVENTS_ENDPOINT}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'DD-API-KEY': `${this.state.api}`,
+            },
+            body: JSON.stringify(this.state.eventBody)
+        })
+        .then(res =>
+            (res.ok) 
+                ? res.json().then(allGood => {
+                console.log(allGood)
+                this.setState({ allGood: true })
+                this.setState({ loading: false })
+            })
+            : res.json().then(resJson=>this.setState({error:resJson.error}))
+        )  
+    }
     
     render() {
         return (
@@ -13,6 +58,7 @@ export default class Event extends React.Component {
                     style={{ textDecoration: 'none' }}
                     to='/'><button>Home</button>
                 </Link>
+                <button onClick={this.handleLogEvent}>send event</button>
             </div>
         )
     }
--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://jsonplaceholder.typicode.com/todos/1',
     LOGS_ENDPOINT: 'https://cors-anywhere.herokuapp.com/https://http-intake.logs.datadoghq.com/v1/input',
-    TEMPLATE_ENDPOINT: 'http://localhost:3187/templates',
-    SAVED_ENDPOINNT: ''
+    EVENTS_ENDPOINT: 'https://cors-anywhere.herokuapp.com/https://api.datadoghq.com/api/v1/events'
 }
